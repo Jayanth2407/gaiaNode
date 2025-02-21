a58a453ad65d2419a8d1ba14f82c63460397cf7e
Jayanth SR <Jayanthdj2407@gmail.com>
2025-02-21 10:12:32 +0530

    Update gaiabotga1.sh

--- gaiabotga1.sh
+++ gaiabotga1.sh
@@ -49,7 +49,7 @@ EOF
             fi
         else
             echo "⚠️ [ERROR] API: $api_url | Status: $http_status | Retrying in 2s..."
-            sleep 2
+            sleep 1
         fi
     done
 }
